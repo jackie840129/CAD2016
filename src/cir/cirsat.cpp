f087dd4ff31e7cfe051129e11cfb5704d0725869
jackie <b02901122@ntu.edu.tw>
2016-06-07 02:03:18 +0800

    sat

--- src/cir/cirsat.cpp
+++ src/cir/cirsat.cpp
@@ -10,6 +10,24 @@ void CirMgr::genProofModel(SatSolver& s){
     }
 }
 void CirMgr::runsat(){
+    SatSolver solver;
+    solver.initialize();
+    genProofModel(solver);
+    /*
+    for(size_t i = 0; i< InputList.size();++i){
+        //做兩次  0->1  & 1->0
+        solver.assertProperty(WireList[0][i]->getVar(),false);
+        solver.assertProperty(WireList[1][i]->getVar(),true);
+        //see outs , solve SAT
+        for(size_t j = 0;j<OutputList.size();++j){
+            // 
+        }
+        //2.
+        solver.assertProperty(WireList[0][i]->getVar(),true);
+        solver.assertProperty(WireList[1][i]->getVar(),false);
+        //see out , solve SAT
+        //backtrack
+        */
 }
 
 
