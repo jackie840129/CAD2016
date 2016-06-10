fa515b5a7c8f4c99612f108efd71b6ccbf36b2ba
jackie <b02901122@ntu.edu.tw>
2016-06-10 23:36:56 +0800

    small update

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -11,11 +11,11 @@ CirMgr* cirMgr = 0;
 int main(int argc, char**argv){
     
     cirMgr = new CirMgr();
-    string file1 = "PD_case/case0/input/cadcontest.v";
+    string file1 = "PD_case/case1/input/cadcontest.v";
     cirMgr->read_module(file1);
-    string file2 = "PD_case/case0/input/case0";
+    string file2 = "PD_case/case1/input/case1";
     cirMgr->read_circuit(file2);
-    cirMgr->set_inputfile("case0");
+    cirMgr->set_inputfile("case1");
     cirMgr->print_information();
     cirMgr->print_io();
   
@@ -23,7 +23,7 @@ int main(int argc, char**argv){
     //cirMgr->print_DFS();
     //string file3 = "PD_case/case3/input/case3.sdc";
     //cirMgr->read_timeconstraint(file3);
-    cirMgr->settime_constraint(10);
+    cirMgr->settime_constraint(45);
     cirMgr->setLayerSize(cirMgr->get_time_constraint());
 
     cout << "Check assign slack in Makefile: "<<slack<<endl;
